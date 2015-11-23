5ae936711d18a6e522d0cd847e460bbc2c2ec8aa
Jo Øivind Gjernes <jodleif@gmail.com>
2015-11-23 16:54:09 +0100

    Test-commit

--- Main.java
+++ Main.java
@@ -7,13 +7,9 @@ package virtualm;
  */
 public class Main
 {
-	public native void assemble(String filename);
 
 	public static void main(String[] args)
 	{
-		System.out.println("Loading library...");
-		System.loadLibrary("virtualm");
-		System.out.println("Running assembler...");
-		new Main().assemble("test.txt");
+		System.out.println("VirtualM");
 	}
 }
